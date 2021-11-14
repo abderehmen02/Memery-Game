2aa09022b0ccf5a5d607ae392920f3c269839c63
abderehmen02 <ilahalaha0987@gmail.com>
2021-11-14 09:50:04 +0100

    second commit

--- src/src (1)/index (1).js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App  from './app.js';
-import {Animated} from "react-animated-css";
-import './App.css'
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
